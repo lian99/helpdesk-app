a2b4911b0e061a7d6782f39600abe97c978a772a
lian <liannatour15@gmail.com>
2026-03-29 19:28:47 +0300

    fix api.js for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,16 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000'
-})
-
-import axios from 'axios'
-
-const api = axios.create({
   baseURL: 'https://helpdesk-backend-ec42.onrender.com'
 })
 
-// Automatically attach token to every request if logged in
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
   if (token) config.headers.Authorization = `Bearer ${token}`
